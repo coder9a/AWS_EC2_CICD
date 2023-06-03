2f598cc28a6163113e85d6353e0a3826cb3c24d7
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2023-06-03 17:12:10 +0530

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -1,44 +1,48 @@
 variable "aws_region" {
-  type = string
-  description = "AWS region"
+  type        = string
+  description = "region where vpn is configured"
 }
+
 variable "aws_access_key" {
-  type = string
-  description = "AWS access key"
+  type        = string
+  description = "Access key of AWS account"
 }
 
 variable "aws_secret_key" {
-  type = string
-  description = "AWS secret key"
+  type        = string
+  description = "secret key of AWS account"
 }
 
 variable "ami" {
-  type = string
+  type        = string
   description = "EC2 instance AMI"
-  default = "ami-0aa2b7722dc1b5612"
+  default     = "ami-0aa2b7722dc1b5612"
 }
 
 variable "instance_type" {
-  type = string
+  type        = string
   description = "AWS instance type"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "vpc_cidr" {
-  type = string
-  description = "range of vpc cidr"
-  default = "10.0.0.0/16"
+  type        = string
+  description = "vpc cidr"
+  default     = "10.0.0.0/16"
 }
 
-variable "public_cidr" {
-  type = string
-  description = "range of public subnet cidr"
-  default = "10.0.1.0/24"
+variable "public_subnet_cidr" {
+  type        = string
+  description = "public subnet cidr"
+  default     = "10.0.1.0/24"
 }
 
-variable "private_cidr" {
-  type = string
-  description = "range of private subnet cidr"
-  default = "10.0.2.0/24"
+variable "private_subnet_cidr" {
+  type        = string
+  description = "private subnet cidr"
+  default     = "10.0.2.0/24"
 }
 
+variable "ports" {
+  type = list(number)
+}
