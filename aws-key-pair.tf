96d8527a67d24ef29385e210002b2184dea41b0e
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2023-06-08 20:44:14 +0530

    modified key pair

--- aws-key-pair.tf
+++ aws-key-pair.tf
@@ -1,14 +1,14 @@
-resource "tls_private_key" "rsa-key" {
+resource "tls_private_key" "dev-rsa-key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "local_file" "foo" {
-  content  = tls_private_key.rsa-key.private_key_openssh
+  content  = tls_private_key.dev-rsa-key.private_key_openssh
   filename = "${var.project}_key.pem"
 }
 
 resource "aws_key_pair" "key-pair" {
   key_name   = "${var.project}-key"
-  public_key = tls_private_key.rsa-key.private_key_openssh
+  public_key = tls_private_key.dev-rsa-key.public_key_openssh
 }
