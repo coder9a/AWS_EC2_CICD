08b1451f9f39ccd45f7354042a2adf0ae4944262
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2023-07-29 14:52:25 +0000

    debug

--- variables.tf
+++ variables.tf
@@ -1,46 +1,55 @@
 variable "AWS_Region" {
   type        = string
   description = "region where vpn is configured"
+  default = ""
 }
 
 variable "aws_access_key" {
   type        = string
   description = "Access key of AWS account"
+  default = ""
 }
 
 variable "aws_secret_key" {
   type        = string
   description = "secret key of AWS account"
+  default = ""
 }
 
 variable "AWS_AMI" {
   type        = string
   description = "EC2 instance AMI"
+  default = ""
 }
 
 variable "EC2_Instance_Type" {
   type        = string
   description = "AWS instance type"
+  default = ""
 }
 
 variable "VPC_CIDR" {
   type        = string
   description = "vpc cidr"
+  default = ""
 }
 
 variable "Public_Subnet_CIDR" {
   type        = string
   description = "public subnet cidr"
+  default = ""
 }
 
 variable "Private_Subnet_CIDR" {
   type        = string
   description = "private subnet cidr"
+  default = ""
 }
 
 variable "project" {
   type        = string
   description = "project name"
+  default = ""
 }
 
 variable "ports" {
@@ -50,8 +59,10 @@ variable "ports" {
 
 variable "Private_Instance_Count" {
   type    = number
+  default = ""
 }
 
 variable "Directory_Path" {
   type    = string
+  default = ""
 }
