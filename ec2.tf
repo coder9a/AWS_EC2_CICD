49faf9f9af28881fd0816eb4d142e12deb398571
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2024-01-23 10:49:17 +0000

    data block name fix

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ data "aws_ami" "ec2_ami" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-20.04-amd64-server-20231207"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-20.04-amd64-server-*"]
   }
 }
 
