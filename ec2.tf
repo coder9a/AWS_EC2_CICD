e3b06cebdba024ca0337009c06d3acbdfa5e4db0
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2024-01-23 10:46:30 +0000

    data block name fix

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ec2_ami" {
   owners           = ["amazon"]
 
   filter {
-    name   = "ubuntu"
+    name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-20.04-amd64-server-20231207"]
   }
 }
